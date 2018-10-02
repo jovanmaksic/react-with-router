0e28ae03ca7e4e56dceff95793deeb256a0a0a1e
jovanmaksic <jovan.maksic@clovertech.com>
2018-10-02 12:58:15 +0200

    remove all

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap.min.css';
-import './index.css';
-import Layout from './Layout';
-
-ReactDOM.render(<Layout />, document.getElementById('root'));
